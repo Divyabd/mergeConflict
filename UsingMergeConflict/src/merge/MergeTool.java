3ecf681060e367ce6e2fee2a29f9a09bcdc81bd5
Divyabd <divyagowda8393@gmail.com>
2021-02-05 07:50:33 +0530

    4thcommit

--- UsingMergeConflict/src/merge/MergeTool.java
+++ UsingMergeConflict/src/merge/MergeTool.java
@@ -3,13 +3,11 @@ package merge;
 public class MergeTool {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		int m=1;
 		int s=2;
 		System.out.println("hello ppls");
-=======
 	int a=1;
->>>>>>> branch 'master' of http://github.com/Divyabd/mergeConflict.git
+
        System.out.println("Hii divay");
 	int b=3;
 	}
