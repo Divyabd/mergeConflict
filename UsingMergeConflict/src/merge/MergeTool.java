ed7b28b675f27ec7dfdcfd5ad40e16ba6278048a
Divyabd <divyagowda8393@gmail.com>
2021-02-05 07:36:43 +0530

    third commit

--- UsingMergeConflict/src/merge/MergeTool.java
+++ UsingMergeConflict/src/merge/MergeTool.java
@@ -3,7 +3,9 @@ package merge;
 public class MergeTool {
 
 	public static void main(String[] args) {
-		
+		int m=1;
+		int s=2;
+		System.out.println("hello ppls");
        System.out.println("Hii divay");
 	}
 
